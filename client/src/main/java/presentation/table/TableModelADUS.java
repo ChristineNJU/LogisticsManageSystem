db4af434f6d2dc08744c085bca8066055aae0b11
ChristineZhang <zxz14@software.nju.edu.cn>
2015-12-05 09:34:44 +0800

    no message

--- client/src/main/java/presentation/table/TableModelADUS.java
+++ client/src/main/java/presentation/table/TableModelADUS.java
@@ -102,13 +102,13 @@ public class TableModelADUS extends AbstractTableModel {
 	
 	public void addEmptyRow(){
 		Vector<String> element = new Vector<String>();
-		int size = 0;
-		try {
-			size = tableValues.get(0).size();
-		}catch(Exception e){
-			size = 0;
-		}
-		for(int i = 0;i < size;i++){
+//		int size = 0;
+//		try {
+//			size = tableValues.get(0).size();
+//		}catch(Exception e){
+//			size = 0;
+//		}
+		for(int i = 0;i < initialColumnCount;i++){
 			element.add(" ");
 		}
 		tableValues.add(element);
