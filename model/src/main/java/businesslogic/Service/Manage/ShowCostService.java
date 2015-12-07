2b8bd30b27eb0adc4aa7cbdcf5782d5f853ba7dc
finominon <475901250@qq.com>
2015-12-07 22:17:13 +0000

    changeInterface

---
+++ model/src/main/java/businesslogic/Service/Manage/ShowCostService.java
@@ -0,0 +1,10 @@
+package businesslogic.Service.Manage;
+
+import java.util.ArrayList;
+
+import VO.CostVO;
+
+public interface ShowCostService {
+
+	public ArrayList<CostVO> showCost();
+}
---
+++ model/src/main/java/businesslogic/Service/Manage/ShowStorageService.java
@@ -0,0 +1,11 @@
+package businesslogic.Service.Manage;
+
+import java.util.ArrayList;
+
+import VO.StorageVO;
+
+public interface ShowStorageService {
+
+	public ArrayList<StorageVO> showStorage();
+	
+}
